c332a18d57f090c1f5613953eb176c921587a11b
pcmbp <pichao314@gmail.com>
2020-02-29 19:42:46 -0800

    cli args test passed

--- tcp/tcp_client.py
+++ tcp/tcp_client.py
@@ -1,6 +1,7 @@
 import logging
 import socket
 import time
+import sys
 
 logging.basicConfig(level=logging.INFO,
                     filename='tcp_client_out.txt',
@@ -12,25 +13,30 @@ TCP_PORT = 5000
 BUFFER_SIZE = 1024
 MESSAGE = "ping"
 
+cid = 'A'
+delay = 1
+count = 10
 
-def send(cid='unknown'):
+
+def send():
     # Create a TCP/IP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect to server
     s.connect((TCP_IP, TCP_PORT))
-    for i in range(10):
+    for i in range(count):
         s.send(f"{cid}:{MESSAGE}".encode())
         data = s.recv(BUFFER_SIZE)
         print(f"received data:{data.decode()}")
         logging.info(f"received data:{data.decode()}")
-        time.sleep(1)
+        time.sleep(delay)
     s.close()
 
 
-def get_client_id():
-    cid = input("Enter client id:")
-    return cid
-
-
 if __name__ == "__main__":
-    send(get_client_id())
+    if len(sys.argv) != 4:
+        print("Wrong arguments!")
+    else:
+        cid = sys.argv[1]
+        delay = int(sys.argv[2])
+        count = int(sys.argv[3])
+        send()
